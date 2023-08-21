0271fbb23c8e4e64e296b26ca3193ba45850534a
B1uskyy <mats.kvanvik@hotmail.com>
2023-08-21 13:55:29 +0200

    added number 0, and rounding to 2 decimals for division

--- script.js
+++ script.js
@@ -12,7 +12,12 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2; 
+
+    if (num2 == 0) alert("Can't divide by 0!");
+
+    else{
+        return (num1 / num2).toFixed(2); 
+    }
 }
 
 
